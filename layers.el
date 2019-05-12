733f90f69797a9a1cee849974b5f823c81381704
Luis Osa <luis.osa.gdc@gmail.com>
2019-05-12 22:33:22 +0200

    Add initial stub

---
+++ layers.el
@@ -0,0 +1,14 @@
+;; layers.el -- Escala layer layers file for Spacemacs
+;;
+;; Copyright (c) 2012-2018 Sylvain Benner & Contributors
+;; Copyright (c) 2019 Luis Osa
+;;
+;; Author: Luis Osa <luis.osa.gdc@gmail.com>
+;; URL:
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(configuration-layer/declare-layer 'lsp)
+
